30c732ae4e3590e019fbd148afb665fa1f7fe4b7
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2025-03-01 12:46:38 -0800

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,29 +1,11 @@
 INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (1, "fruit", "Apple", 0.89, 0.33, 200);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (2, "fruit", "Banana", 0.44, 0.26, 300);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (3, "fruit", "Orange", 0.99, 0.6, 200);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (4, "fruit", "Strawberry", 0.22, 0.06, 400);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (5, "vegetable", "Lettuce", 1.89, 0.66, 100);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (6, "vegetable", "Potato", 0.79, 0.47, 100);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (7, "vegetable", "Carrot", 0.99, 0.16, 100);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (8, "vegetable", "Onion", 0.84, 0.55, 100);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
-VALUES (9, "grain", "White Bread (Loaf)", 2.49, 1.00, 200);
-
-INSERT INTO Product (productID, category, name, price, pounds, quantity)
+VALUES (1, "fruit", "Apple", 0.89, 0.33, 200),
+VALUES (2, "fruit", "Banana", 0.44, 0.26, 300),
+VALUES (3, "fruit", "Orange", 0.99, 0.6, 200),
+VALUES (4, "fruit", "Strawberry", 0.22, 0.06, 400),
+VALUES (5, "vegetable", "Lettuce", 1.89, 0.66, 100),
+VALUES (6, "vegetable", "Potato", 0.79, 0.47, 100),
+VALUES (7, "vegetable", "Carrot", 0.99, 0.16, 100),
+VALUES (8, "vegetable", "Onion", 0.84, 0.55, 100),
+VALUES (9, "grain", "White Bread (Loaf)", 2.49, 1.00, 200),
 VALUES (10, "grain", "White Rice (1 Cup)", 0.34, 0.43, 200);
