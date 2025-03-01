c755fa73db71ad44b00b09b7d3d5efa24cabb913
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2025-03-01 11:49:18 -0800

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,1 +1,11 @@
+INSERT INTO Product (productID, category, name, price, pounds, quantity)
+VALUES (1, "fruit", "Apple", 0.89, 0.33, 200);
 
+INSERT INTO Product (productID, category, name, price, pounds, quantity)
+VALUES (2, "fruit", "Banana", 0.44, 0.26, 300);
+
+INSERT INTO Product (productID, category, name, price, pounds, quantity)
+VALUES (3, "fruit", "Orange", 0.99, 0.6, 200);
+
+INSERT INTO Product (productID, category, name, price, pounds, quantity)
+VALUES (4, "fruit", "Strawberry", 0.22, 0.06, 400);
