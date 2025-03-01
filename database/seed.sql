42518d452e65e2a4461fb1ac75498f16f51a7fab
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2025-03-01 13:22:33 -0800

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -9,3 +9,12 @@ VALUES (7, "vegetable", "Carrot", 0.99, 0.16, 100),
 VALUES (8, "vegetable", "Onion", 0.84, 0.55, 100),
 VALUES (9, "grain", "White Bread (Loaf)", 2.49, 1.00, 200),
 VALUES (10, "grain", "White Rice (1 Cup)", 0.34, 0.43, 200);
+
+INSERT INTO User (userID, isAdmin, firstName, lastName, email, hashedPassword)
+VALUES (1, 1, "Danny", "Xu", "pocafup@gmail.com", "JEGI23jGEIU23jiweg23FJE"),
+VALUES (2, 1, "Kevin", "Chan", "Endlessvoid@gmail.com", "EYg1328EGEGJbw238FEig"),
+VALUES (11, 0, "Alice", "Anderson", "AIEADRO@gmail.com", "KDdF23psI4FJSqlJ485f"),
+VALUES (12, 0, "Bob", "Brandon", "BobTheLocalCook@gmail.com", "a21sBbq0pQShjz7sEf89"),
+VALUES (13, 0, "Charlie", "Copper", "CharliePersonal@gmail.com", "6etrFU7sx7hScWld4eiv"),
+VALUES (14, 0, "Margaret", "Marcus", "MMWorkLife@gmail.com", "7Bqwuv43SAFSdjs23PTd");
+
