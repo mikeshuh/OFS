885767d582761b5df547e7b34f5dc9bead04c75d
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2025-03-01 11:06:18 -0800

    Update view.sql

--- database/view.sql
+++ database/view.sql
@@ -1,1 +1,15 @@
+CREATE VIEW User_view AS
+SELECT userID, isAdmin, firstName, lastName, email, hashedPassword 
+FROM User;
 
+CREATE VIEW Order_view AS
+SELECT orderID, userID, totalPrice, totalPounds, deliveryFee, orderTime, orderStatus, streetAddress, city, zipCode 
+FROM Order;
+
+CREATE VIEW Product_view AS
+SELECT productID, category, name, price, pounds, quantity 
+FROM Product;
+
+CREATE VIEW OrderProduct_view AS
+SELECT orderProductID, orderID, productID, quantity 
+FROM OrderProduct;
