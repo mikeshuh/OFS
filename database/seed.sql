9122f46b78a37f887c9bc35b8afe4d9b567f6098
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2025-03-01 13:25:47 -0800

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,20 +1,20 @@
 INSERT INTO Product (productID, category, name, price, pounds, quantity)
 VALUES (1, "fruit", "Apple", 0.89, 0.33, 200),
-VALUES (2, "fruit", "Banana", 0.44, 0.26, 300),
-VALUES (3, "fruit", "Orange", 0.99, 0.6, 200),
-VALUES (4, "fruit", "Strawberry", 0.22, 0.06, 400),
-VALUES (5, "vegetable", "Lettuce", 1.89, 0.66, 100),
-VALUES (6, "vegetable", "Potato", 0.79, 0.47, 100),
-VALUES (7, "vegetable", "Carrot", 0.99, 0.16, 100),
-VALUES (8, "vegetable", "Onion", 0.84, 0.55, 100),
-VALUES (9, "grain", "White Bread (Loaf)", 2.49, 1.00, 200),
-VALUES (10, "grain", "White Rice (1 Cup)", 0.34, 0.43, 200);
+(2, "fruit", "Banana", 0.44, 0.26, 300),
+(3, "fruit", "Orange", 0.99, 0.6, 200),
+(4, "fruit", "Strawberry", 0.22, 0.06, 400),
+(5, "vegetable", "Lettuce", 1.89, 0.66, 100),
+(6, "vegetable", "Potato", 0.79, 0.47, 100),
+(7, "vegetable", "Carrot", 0.99, 0.16, 100),
+(8, "vegetable", "Onion", 0.84, 0.55, 100),
+(9, "grain", "White Bread (Loaf)", 2.49, 1.00, 200),
+(10, "grain", "White Rice (1 Cup)", 0.34, 0.43, 200);
 
 INSERT INTO User (userID, isAdmin, firstName, lastName, email, hashedPassword)
 VALUES (1, 1, "Danny", "Xu", "pocafup@gmail.com", "JEGI23jGEIU23jiweg23FJE"),
-VALUES (2, 1, "Kevin", "Chan", "Endlessvoid@gmail.com", "EYg1328EGEGJbw238FEig"),
-VALUES (11, 0, "Alice", "Anderson", "AIEADRO@gmail.com", "KDdF23psI4FJSqlJ485f"),
-VALUES (12, 0, "Bob", "Brandon", "BobTheLocalCook@gmail.com", "a21sBbq0pQShjz7sEf89"),
-VALUES (13, 0, "Charlie", "Copper", "CharliePersonal@gmail.com", "6etrFU7sx7hScWld4eiv"),
-VALUES (14, 0, "Margaret", "Marcus", "MMWorkLife@gmail.com", "7Bqwuv43SAFSdjs23PTd");
+(2, 1, "Kevin", "Chan", "Endlessvoid@gmail.com", "EYg1328EGEGJbw238FEig"),
+(11, 0, "Alice", "Anderson", "AIEADRO@gmail.com", "KDdF23psI4FJSqlJ485f"),
+(12, 0, "Bob", "Brandon", "BobTheLocalCook@gmail.com", "a21sBbq0pQShjz7sEf89"),
+(13, 0, "Charlie", "Copper", "CharliePersonal@gmail.com", "6etrFU7sx7hScWld4eiv"),
+(14, 0, "Margaret", "Marcus", "MMWorkLife@gmail.com", "7Bqwuv43SAFSdjs23PTd");
 
