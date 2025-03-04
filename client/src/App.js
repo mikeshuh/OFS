453135a2607b6d0b6ef80a616e3679a2a24ff475
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2025-03-03 23:45:24 -0800

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
+import Products from "./pages/Products"
+import Profile from "./pages/Profile";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/categories" element={<div>Food Category Page</div>} />
         <Route path="/cart" element={<div>Shopping Cart Page</div>} />
-        <Route path="/profile" element={<div>User Profile Page</div>} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<div>Login Page</div>} />
         <Route path="/signup" element={<div>Sign Up Page</div>} />
         <Route path="/products" element={<div>Products</div>} />
