d47b5978408bf33a596a341ab9d5993aea66ccb5
h5mota <pocafup@gmail.com>
2025-03-04 19:51:30 -0800

    Update Code

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
+
+function Home() {
+  return (
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -7,11 +7,11 @@ function Login() {
       <header className="Login-header">
         <h1>New User Login</h1>
         <form>
-          <div className="form-group" class="sr-only">
+          <div className="form-group" >
             <label htmlFor="username">Username:</label>
             <input type="text" id="username" name="username" placeholder="username" />
           </div>
-          <div className="form-group" class="sr-only">
+          <div className="form-group" >
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" placeholder="password" />
           </div>
