096557081de445516f486b4df52d789c9c012117
h5mota <pocafup@gmail.com>
2025-03-04 21:20:59 -0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products"
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+
+import Signup from "./pages/Signup";
 import "./App.css";
 
 function App() {
@@ -14,8 +17,8 @@ function App() {
         <Route path="/categories" element={<div>Food Category Page</div>} />
         <Route path="/cart" element={<div>Shopping Cart Page</div>} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<div>Login Page</div>} />
-        <Route path="/signup" element={<div>Sign Up Page</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/products" element={<div>Products</div>} />
       </Routes>
     </Router>
