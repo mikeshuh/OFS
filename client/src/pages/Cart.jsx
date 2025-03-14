c4d08c6d5af973256e03307a841d4b3132121236
Timothy Nguyen <minchkin314@gmail.com>
2025-03-14 16:31:32 -0700

    Updated Cart, Checkout, and both Order pages.

---
+++ client/src/pages/Cart.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import DiscountBanner from "../components/DiscountBanner";
+import Navbar from "../components/Navbar.jsx";
+
+function ShoppingCart() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    alert(`Searching for: ${searchQuery}`);
+};
+
+const [viewMode, setView] = useState(true);
+    const EditButton = () => {
+            setView(!viewMode);
+    };
+
+const toCheckout = () => {
+  window.location.href="./checkout";
+};
+
+const discardCart = () => {
+  alert("This function should cancel shopping");
+};
+
+  return (
+    <div classname="flex flex-col min-h-screen">
+      {/* Navi Bar */}
+      <Navbar />
+
+      {/* Discount */}
+      <DiscountBanner />
+
+      {/* Main page information */}
+      <div className="flex-1 flex-col items-center justify-center text-center p-12">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Shopping Cart</h1>
+        
+        <table className="mx-auto border-collapse border-[3px] border-solid border-green-500 font-size: 16px">
+        <tr className="border-[#28a745]">
+            <th className="w-[150px] text-white bg-green-500">Item</th>
+            <th className="w-[150px] text-white bg-green-500">Individual Cost</th>
+            <th className="w-[150px] text-white bg-green-500">Individual Pounds</th>
+            <th className="w-[150px] text-white bg-green-500">Quantity</th>
+        </tr>
+        <tr className="border-[#28a745]">
+            <td className="w-[150px] border border-solid border-green-500">Apple</td>
+            <td className="w-[150px] border border-solid border-green-500">0.89</td>
+            <td className="w-[150px] border border-solid border-green-500">0.33</td>
+            <td className="w-[150px] border border-solid border-green-500">12
+            {!viewMode && <input
+              type="number"
+              placeholder="Num"
+              min="0"
+              max="999"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="ml-[10px] border w-[70px]
+              px-3 py-2 border-solid border-[#ddd]"/>}
+            </td>
+        </tr>
+        </table>
+        
+        {viewMode && <button className="m-[10px] bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300" onClick={toCheckout} >
+          Purchase
+        </button>}
+        
+        {viewMode && <button className="m-[10px] bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300" onClick={EditButton}>
+          Edit Cart
+        </button>}
+
+        {viewMode && <button className="m-[10px] bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300" onClick={discardCart}>
+          Discard Cart
+        </button>}
+
+        {!viewMode && <button className="m-[10px] bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300" onClick={EditButton}>
+          Save Changes
+        </button>}
+
+        {!viewMode && <button className="m-[10px] bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300" onClick={EditButton}>
+          Cancel Changes
+        </button>}
+
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingCart;
---
+++ client/src/pages/OrderDetails.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import DiscountBanner from "../components/DiscountBanner";
+import Navbar from "../components/Navbar.jsx";
+
+function Orders() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    alert(`Searching for: ${searchQuery}`);
+};
+
+const [viewMode, setView] = useState(true);
+    const EditButton = () => {
+            setView(!viewMode);
+    };
+
+const toOrder = () => {
+  window.location.href="./orders";
+}
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Navi Bar */}
+      <Navbar />
+
+      {/* Discount */}
+      <DiscountBanner />
+
+      {/* Main page information */}
+      <div className="flex-1 flex-col items-center justify-center text-center p-12">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Order Details</h1>
+
+        <table className="mx-auto border-collapse border-[3px] border-solid border-green-500 font-size: 16px">
+        <tr className="border-[#28a745]">
+            <th className="w-[150px] text-white bg-green-500">Order ID</th>
+            <th className="w-[150px] text-white bg-green-500">Total Cost</th>
+            <th className="w-[150px] text-white bg-green-500">Total Pounds</th>
+            <th className="w-[150px] text-white bg-green-500">Devlivery Fee?</th>
+            <th className="w-[150px] text-white bg-green-500">Date</th>
+            <th className="w-[150px] text-white bg-green-500">Status</th>
+            <th className="w-[150px] text-white bg-green-500">Address</th>
+            <th className="w-[150px] text-white bg-green-500">City</th>
+            <th className="w-[150px] text-white bg-green-500">Zip Code</th>
+        </tr>
+        <tr className="border-[#28a745]">
+            <td className="w-[150px] border border-solid border-green-500">1</td>
+            <td className="w-[150px] border border-solid border-green-500">10.68</td>
+            <td className="w-[150px] border border-solid border-green-500">3.96</td>
+            <td className="w-[150px] border border-solid border-green-500">Yes</td>
+            <td className="w-[150px] border border-solid border-green-500">2025-03-01 12:54:39</td>
+            <td className="w-[150px] border border-solid border-green-500">Delivered</td>
+            <td className="w-[150px] border border-solid border-green-500">2 East William Street</td>
+            <td className="w-[150px] border border-solid border-green-500">San Jose</td>
+            <td className="w-[150px] border border-solid border-green-500">95112</td>
+        </tr>
+        </table>
+
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Cart Details</h1>
+        <table className="mx-auto border-collapse border-[3px] border-solid border-green-500 font-size: 16px">
+        <tr className="border-[#28a745]">
+            <th className="w-[150px] text-white bg-green-500">Product</th>
+            <th className="w-[150px] text-white bg-green-500">Amount</th>
+            <th className="w-[150px] text-white bg-green-500">Total Cost</th>
+            <th className="w-[150px] text-white bg-green-500">Total Pounds</th>
+        </tr>
+        <tr className="border-[#28a745]">
+            <td className="w-[150px] border border-solid border-green-500">Apple</td>
+            <td className="w-[150px] border border-solid border-green-500">12</td>
+            <td className="w-[150px] border border-solid border-green-500">10.68</td>
+            <td className="w-[150px] border border-solid border-green-500">3.96</td>
+        </tr>
+        </table>
+        
+        <br/>
+
+        <button className="m-[10px] bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-3 rounded-lg transition-colors duration-300" onClick={toOrder}>
+                Back
+            </button>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
