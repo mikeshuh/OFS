8ca3edb51f5495fdbbbb035795906165d2824486
h5mota <pocafup@gmail.com>
2025-03-13 23:00:51 -0700

    saved auth

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -43,7 +43,6 @@ const protect = async (req, res, next) => {
     // Attach user to request object
     req.user = user;
     req.token = token;
-    req.isAdmin = user.isAdmin;
     next();
   } catch (error) {
     console.error('Auth middleware error:', error);
