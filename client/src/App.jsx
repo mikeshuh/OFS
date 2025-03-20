a1fa5c3c49a2294d66095522f150b56514ee69a1
Timothy Nguyen <minchkin314@gmail.com>
2025-03-19 18:41:32 -0700

    Edited App.jsx.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,14 +26,15 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/productDetails" element={<ProductDetails />} />
         <Route path="/categories" element={<div>Food Category Page</div>} />
-
+        
+        <Route element={<ProtectedRoute />}>
           <Route path="/cart" element={<Cart />} />
           <Route path="/profile" element={<Profile />} />
         
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/orderDetails" element={<OrderDetails />} />
-
+        </Route>
 
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
