5ff0f2111a649d0aa9d4e6993fc70810d59b297b
An Ho <an.nguyen.thein.ho@gmail.com>
2025-03-26 15:34:39 -0700

    Updated function createOrder added function getOrderDetailsByOrderID
    createOrder: Now creates ordersProducts
    getOrderDetailsByID: Returns an array of containing product information and product quantity

--- server/src/controllers/orderController.js
+++ server/src/controllers/orderController.js
@@ -1,11 +1,38 @@
 const Order = require('../models/orderModel');
+const { addProductToOrder } = require('../models/orderProductModel');
+const Product = require('../models/productModel');
+const OrderProduct = require('../models/orderProductModel');
 const responseHandler = require('../utils/responseHandler');
+//currently hardcoded but within a variable to get rid of magic numbers
+const deliveryFeeAmount = 10;
 
 const createOrder = async (req, res) => {
   try{
-    const {totalPrice, totalPounds, deliveryFee, orderStatus, streetAddress, city, zipCode} = req.body;
-    const userID = req.user.userID;
 
+    const {streetAddress, city, zipCode, orderProducts} = req.body
+    const userID = req.user.userID;
+    const orderStatus = 0; // 0 pending 1 delivered
+
+    let totalPounds = 0;
+    let totalPrice = 0;
+
+    //calculate total pounds and total price
+    for (const orderProduct of orderProducts) {
+      const productDetails = await Product.findById(orderProduct.productID);
+      if (!productDetails) {
+        return responseHandler.error(res, `Product with ID ${orderProduct.productID} not found.`);
+      }
+      totalPrice += productDetails.price * orderProduct.quantity;
+      totalPounds += productDetails.pounds * orderProduct.quantity;
+    }
+
+    //determine if there is a delivery fee
+    if(totalPounds < 20){
+      deliveryFee = 1;
+      totalPrice = totalPrice + deliveryFeeAmount;
+    }
+
+    //create the order
     const orderData = {
       userID: userID,
       totalPrice: totalPrice,
@@ -17,11 +44,21 @@ const createOrder = async (req, res) => {
       zipCode: zipCode
     }
 
+    //create the order
     const orderID = await Order.create(orderData);
+
     if(!orderID){
       return responseHandler.error(res, 'Error creating order');
     }
-    return responseHandler.created(res, { orderID}, 'Order created succesfully');
+
+    //create OrderProducts
+    const orderProductSuccess = await OrderProduct.addProductsToOrder(orderID, orderProducts);
+
+    if(!orderProductSuccess){
+      return responseHandler.error(res, 'Error creating orderProduct entry');
+    }
+    return responseHandler.created(res, {orderID}, 'Order created succesfully');
+
 
   } catch (error) {
     console.error(`Create order error: ${error.message}`, error);
@@ -45,8 +82,38 @@ const getOrderByUserID = async (req, res) =>{
   }
 }
 
+ // get single order, route: /api/order/:orderId
+ const getOrderDetailsByOrderID = async (req, res) =>{
+  try {
+    //find order by orderId to see if user should be able to access the order
+    const orderID = req.params.orderID;
+    const order = await Order.findById(orderID);
+    const userID = req.user.userID;
+
+    if (!order) {
+      return responseHandler.notFound(res, 'Order not found.');
+    }
+
+    if(order.userID != userID) {
+      return responseHandler.badRequest(res, 'Order not associated with account');
+    }
+
+    //if the user is associated with the order return the associated order products
+    const orderProduct = await OrderProduct.findByOrderId(orderID);
+
+    if (!orderProduct) {
+      return responseHandler.notFound(res, 'OrderProducts not found.');
+    }
+    return responseHandler.success(res, orderProduct, 'Order retrieved succesfully');
+} catch (error) {
+    console.error(`Get order error: ${error.message}`, error);
+    return responseHandler.error(res, 'Failed to retrieve order.');
+  }
+}
+
 
 module.exports = {
   getOrderByUserID,
-  createOrder
+  createOrder,
+  getOrderDetailsByOrderID
 };
--- server/src/routes/orderRoute.js
+++ server/src/routes/orderRoute.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
-const { validateOrder } = require('../utils/validationUtils');
+const { validateOrder, validateParamInt } = require('../utils/validationUtils');
 
 //Creates an order that is associated with the user that is logged in.
 router.post('/create-order', protect, validateOrder, orderController.createOrder);
 
 //Gets the orders associated with the user that is logged in.
 router.get('/user', protect, orderController.getOrderByUserID);
+
+//Gets the order and associated order-products given an orderID and the userID matches the userID on the order
+router.get('/details/:orderID', protect, validateParamInt('orderID'), orderController.getOrderDetailsByOrderID);
 
 module.exports = router;
