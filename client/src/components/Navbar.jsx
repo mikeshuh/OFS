b7859e4c69c8b8d9b2c6be070c68cadd1dc28342
Michael Huh <michael.stephen.huh@gmail.com>
2025-04-22 02:04:11 -0700

    email href (#46)
    * email href
    
    * svg update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -118,12 +118,16 @@ function Navbar() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 6h14a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2z"
               />
             </svg>
             <div className="ml-2">
               <div className="text-sm text-gray-600">Contact Us:</div>
-              <div className="text-sm font-medium">(+01) 111-111-1111</div>
+              <div className="text-sm font-medium">
+                <a href="mailto:sj.ofs.official@gmail.com" className="hover:underline">
+                  sj.ofs.official@gmail.com
+                </a>
+              </div>
             </div>
           </div>
 
